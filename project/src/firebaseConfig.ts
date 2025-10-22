622d2b4371e4d89f5fea5a1f35752b1de24c2df8
MITCHCABRERA <162237774+MITCHCABRERA@users.noreply.github.com>
2025-10-22 18:37:00 +0700

    Update Firebase configuration for new project
    Updated Firebase configuration with new project details.

--- project/src/firebaseConfig.ts
+++ project/src/firebaseConfig.ts
@@ -1,17 +1,25 @@
+// firebaseConfig.js (or firebase.js / authAPI.js)
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOuLXVpLWPP8x8k_UQY-kW0FSrPDjEUJQ",
-  authDomain: "mmdcmindcare-93539.firebaseapp.com",
-  projectId: "mmdcmindcare-93539",
-  storageBucket: "mmdcmindcare-93539.appspot.com",
-  messagingSenderId: "244950654009",
-  appId: "1:244950654009:web:2241ffd147377b269841e8",
-  measurementId: "G-LYZFJ93XJ6"
+  apiKey: "AIzaSyDGvmKTGpLSK_xDDHO5hIf2ztZhXmEJLD4",
+  authDomain: "mmdcmindcare-26a53.firebaseapp.com",
+  projectId: "mmdcmindcare-26a53",
+  storageBucket: "mmdcmindcare-26a53.firebasestorage.app",
+  messagingSenderId: "14628770249",
+  appId: "1:14628770249:web:41468d476ad564c92d1818",
+  measurementId: "G-66764Y3N01"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 
+// Initialize Auth + Google Provider
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+// Force Google to ask user which account to use
+googleProvider.setCustomParameters({ prompt: "select_account" });
